9e2d71884e6a3b2406d92f59d5a451fed8394085
RGonzalezs <evilholic@hotmail.com>
2017-05-31 17:10:19 -0500

    NewMusicApp
    Create Repo

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cooverRadioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cooverRadioApp
+ */
+angular.module('cooverRadioApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cooverRadioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cooverRadioApp
+ */
+angular.module('cooverRadioApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
